2403df82d8d7fe6dcbc837f4d5a318620cf116c6
FergusPrimus <fergusprimus@gmail.com>
2021-02-04 14:02:38 -0700

    Added about team and tags pages. Changed layout and added navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import '../styles/index.scss'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,13 +26,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div className="container" id="content">
         <main>{children}</main>
         <footer
           style={{
