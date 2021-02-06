45ec6430f2346f932feeae2748eb3459adbc81f3
FergusPrimus <fergusprimus@gmail.com>
2021-02-06 13:39:56 -0700

    Filled in the tags page.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,55 +1,88 @@
-const { slugify } = require('./src/util/utilityFunctions');
-const path = require('path')
-const authors = require('./src/util/authors')
+const { slugify } = require("./src/util/utilityFunctions")
+const path = require("path")
+const authors = require("./src/util/authors")
+const _ = require("lodash")
 
 exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions
-    console.log(node.internal.type)
-    if(node.internal.type === 'MarkdownRemark'){
-        const slugFromTitle = slugify(node.frontmatter.title)
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slugFromTitle
-        })
-    }
+  const { createNodeField } = actions
+  console.log(node.internal.type)
+  if (node.internal.type === "MarkdownRemark") {
+    const slugFromTitle = slugify(node.frontmatter.title)
+    createNodeField({
+      node,
+      name: "slug",
+      value: slugFromTitle,
+    })
+  }
 }
 
 exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions;
-    const singlePostTemplate = path.resolve('src/templates/single-post.js')
+  const { createPage } = actions
 
-    return graphql(`
-        {
-            allMarkdownRemark{
-                edges{
-                    node{
-                        frontmatter{
-                            author
-                        }
-                        fields{
-                            slug
-                        }
-                    }
-                }
+  const templates = {
+      singlePost: path.resolve("src/templates/single-post.js"),
+      tagsPage: path.resolve("src/templates/tags-page.js")
+  }
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              author
+              tags
             }
+            fields {
+              slug
+            }
+          }
         }
-    `).then(res => {
-        if(res.errors) return Promise.reject(res.errors)
+      }
+    }
+  `).then(res => {
+    if (res.errors) return Promise.reject(res.errors)
 
-        const posts = res.data.allMarkdownRemark.edges
+    const posts = res.data.allMarkdownRemark.edges
 
-        posts.forEach(({ node }) => {
-            createPage({
-                path: node.fields.slug,
-                component: singlePostTemplate,
-                context: {
-                    // Passing slug for template to use to get post
-                    slug: node.fields.slug,
-                    // Find author imageUrl from authors and pass it to the single post template
-                    imageUrl: authors.find(x => x.name === node.frontmatter.author).imageUrl
-                }
-            })
-        })
+    posts.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: templates.singlePost,
+        context: {
+          // Passing slug for template to use to get post
+          slug: node.fields.slug,
+          // Find author imageUrl from authors and pass it to the single post template
+          imageUrl: authors.find(x => x.name === node.frontmatter.author)
+            .imageUrl,
+        },
+      })
     })
+
+    // Create and populate an array of all tags
+    let tags = []
+    _.each(posts, edge => {
+        if(_.get(edge, 'node.frontmatter.tags')){
+            tags = tags.concat(edge.node.frontmatter.tags)
+        }
+    })
+
+    // Create an object containing the number of times a tag appears.
+    let tagPostCounts = {}
+    tags.forEach(tag => {
+        tagPostCounts[tag] = (tagPostCounts[tag] || 0) +1;
+    })
+
+    // Eliminate duplicate entries
+    tags = _.uniq(tags)
+
+    createPage({
+        path: `/tags`,
+        component: templates.tagsPage,
+        context: {
+          tags,
+          tagPostCounts
+        }
+    })
+  })
 }
