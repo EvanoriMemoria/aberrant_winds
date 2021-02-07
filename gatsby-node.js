89cb429631c7de760b68a18cb1dec90b03505f90
FergusPrimus <fergusprimus@gmail.com>
2021-02-07 10:50:27 -0700

    Added pagination for posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,7 +22,8 @@ exports.createPages = ({ actions, graphql }) => {
   const templates = {
       singlePost: path.resolve("src/templates/single-post.js"),
       tagsPage: path.resolve("src/templates/tags-page.js"),
-      tagposts: path.resolve("src/templates/tag-posts.js")
+      tagposts: path.resolve("src/templates/tag-posts.js"),
+      postList: path.resolve("src/templates/post-list.js"),
   }
 
   return graphql(`
@@ -93,6 +94,26 @@ exports.createPages = ({ actions, graphql }) => {
         component: templates.tagposts,
         context: {
           tag,
+        }
+      })
+    })
+
+    const postsPerPage = 2
+    const numberOfPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numberOfPages }).forEach((_, index) => {
+      const isFirstPage = index === 0
+      const currentPage = index + 1
+
+      if(isFirstPage) return
+
+      createPage({
+        path: `/page/${currentPage}`,
+        component: templates.postList,
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          currentPage
         }
       })
     })
