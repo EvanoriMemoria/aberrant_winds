f4c33caf095cca74ce983ef42baef09656abd56f
FergusPrimus <fergusprimus@gmail.com>
2021-02-07 11:13:25 -0700

    Added pagination links.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,33 +3,43 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql, StaticQuery } from "gatsby"
 import Post from "../components/Post"
+import PaginationLinks from "../components/PaginationLinks"
 
-const IndexPage = () => (
-  <Layout pageTitle="Welcome to Aberrant Winds!">
-    <SEO title="Home" />
-    <StaticQuery
-      query={indexQuery}
-      render={data => {
-        return (
-          <div>
-            {data.allMarkdownRemark.edges.map(({ node }) => (
-              <Post
-                key={node.id}
-                title={node.frontmatter.title}
-                author={node.frontmatter.author}
-                slug={node.fields.slug}
-                date={node.frontmatter.date}
-                body={node.excerpt}
-                fluid={node.frontmatter.image.childImageSharp.fluid}
-                tags={node.frontmatter.tags}
-              />
-            ))}
-          </div>
-        )
-      }}
-    />
-  </Layout>
-)
+const IndexPage = () => {
+  const postsPerPage = 2
+  let numberOfPages
+
+  return (
+    <Layout pageTitle="Welcome to Aberrant Winds!">
+      <SEO title="Home" />
+      <StaticQuery
+        query={indexQuery}
+        render={data => {
+          numberOfPages = Math.ceil(
+            data.allMarkdownRemark.totalCount / postsPerPage
+          )
+          return (
+            <div>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Post
+                  key={node.id}
+                  title={node.frontmatter.title}
+                  author={node.frontmatter.author}
+                  slug={node.fields.slug}
+                  date={node.frontmatter.date}
+                  body={node.excerpt}
+                  fluid={node.frontmatter.image.childImageSharp.fluid}
+                  tags={node.frontmatter.tags}
+                />
+              ))}
+              <PaginationLinks currentPage={1} numberOfPages={numberOfPages} />
+            </div>
+          )
+        }}
+      />
+    </Layout>
+  )
+}
 
 const indexQuery = graphql`
   query {
@@ -37,6 +47,7 @@ const indexQuery = graphql`
       sort: { fields: [frontmatter___date], order: DESC }
       limit: 2
     ) {
+      totalCount
       edges {
         node {
           id
