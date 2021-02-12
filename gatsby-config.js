c1ce6b318647753525897f0ee848a5fd00af2455
FergusPrimus <fergusprimus@gmail.com>
2021-02-12 16:20:51 -0700

    Updated the description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Aberrant Winds`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Minecraft servers hosted for the Aberrant Winds community!`,
+    author: `@fergenbergel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
