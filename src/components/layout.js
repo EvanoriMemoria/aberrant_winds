6262a35e0d9e4f79a663faf714a71b784cd96fa3
Nic Rust <fergenbergel@aberrantwinds.xyz>
2023-07-20 15:48:44 -0600

    Added banner and update donors page

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@ import Footer from "./Footer"
 import "../styles/index.scss"
 import { Row, Col } from "reactstrap"
 import Sidebar from "./Sidebar"
+import banner from "../images/banner.png"
 
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
@@ -30,7 +31,10 @@ const Layout = ({ authorImageFluid, children, pageTitle, postAuthor, pageId }) =
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div className="container" id="content">
-        <h1>{pageTitle}</h1>
+          <div class={pageId === "index" ? "container2" : "container3"}>
+            <h1 class="centered-title"> {pageTitle}</h1>
+            <img class="banner" src={banner} alt="Aberrant Winds Banner"/>
+          </div>
         <Row>
           <Col md="8">{children}</Col>
           <Col md="4">
